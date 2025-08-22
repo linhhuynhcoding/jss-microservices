d339d3635e64a06932937e5de9acfd49a7f19914
linhhuynhcoding <nhatlinh.6449@gmail.com>
2025-08-22 13:44:09 +0000

    ...feat: implement wrapper rabbit mq client

---
+++ mq/consts/keys.go
@@ -0,0 +1,17 @@
+package consts
+
+// COMMON
+var ()
+
+// TOPIC ROUTING KEYS
+var (
+	// PRODUCT_CUSTOMER_SERVICE
+	TOPIC_CUSTOMER_BROADCAST string = "customer.*"
+	TOPIC_CREATE_CUSTOMER    string = "customer.create_customer"
+
+	TOPIC_PRODUCT_BROADCAST string = "product.*"
+	TOPIC_CREATE_PRODUCT    string = "product.create_product"
+)
+
+// DIRECT ROUTING KEYS
+var ()
---
+++ mq/go.mod
@@ -0,0 +1,11 @@
+module github.com/linhhuynhcoding/jss-microservices/mq
+
+go 1.24.6
+
+require (
+	github.com/rabbitmq/amqp091-go v1.10.0
+	go.uber.org/zap v1.27.0
+	google.golang.org/protobuf v1.36.8
+)
+
+require go.uber.org/multierr v1.10.0 // indirect
